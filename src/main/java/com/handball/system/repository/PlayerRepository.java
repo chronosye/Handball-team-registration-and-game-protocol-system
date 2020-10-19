0fc5a65f26b4da63e22d2bc8f21d309fcdacf27b
Uldis Jansons <jansons08@gmail.com>
2020-10-19 17:32:11 +0300

    Updated repositories

--- src/main/java/com/handball/system/repository/PlayerRepository.java
+++ src/main/java/com/handball/system/repository/PlayerRepository.java
@@ -1,9 +1,13 @@
 package com.handball.system.repository;
 
 import com.handball.system.entity.Player;
+import com.handball.system.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Long> {
+    Set<Player> findPlayersByTeam(Team team);
 }
