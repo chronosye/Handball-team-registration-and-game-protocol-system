3aa5e04bf760474b967aef8dec255a84d08800f3
Uldis Jansons <jansons08@gmail.com>
2020-10-27 17:25:26 +0200

    added admin controller

---
+++ src/main/java/com/handball/system/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.handball.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminPanel(){
+        return "admin";
+    }
+}
