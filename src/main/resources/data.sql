ec78aa5561c90fa7622d38c5eb3830b39c1e74bf
Uldis Jansons <jansons08@gmail.com>
2020-10-29 17:24:29 +0200

    added more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 insert into user (id,email,name,password,surname) values ('1','admin@test','Admins','$2a$15$O2Kdn.QojtG1mFWdGo97FuqK9moL.S29TjOJBLZCsOpJ6kE2p2uVa','Test')
 insert into user (id,email,name,password,surname) values ('2','user@test','User','$2a$15$O2Kdn.QojtG1mFWdGo97FuqK9moL.S29TjOJBLZCsOpJ6kE2p2uVa','UserTest')
+insert into user (id,email,name,password,surname) values ('3','org@test','Org','$2a$15$O2Kdn.QojtG1mFWdGo97FuqK9moL.S29TjOJBLZCsOpJ6kE2p2uVa','OrgTest')
 
 insert into user_roles(user_id,role) values ('1','ADMIN')
 insert into user_roles(user_id,role) values ('2','USER')
+insert into user_roles(user_id,role) values ('3','ORGANIZER')
