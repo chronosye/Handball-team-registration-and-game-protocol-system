2d235cfa7dbacb02df1bb49eb38bb56dae745f35
Uldis Jansons <jansons08@gmail.com>
2020-11-02 16:02:32 +0200

    added that manager can add players after creating team. And that can edit and delete player

--- src/main/java/com/handball/system/controller/ManagerController.java
+++ src/main/java/com/handball/system/controller/ManagerController.java
@@ -3,12 +3,14 @@ package com.handball.system.controller;
 import com.handball.system.entity.Player;
 import com.handball.system.entity.Team;
 import com.handball.system.entity.User;
+import com.handball.system.service.PlayerService;
 import com.handball.system.service.TeamService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,32 +21,77 @@ import javax.validation.Valid;
 public class ManagerController {
 
     private final TeamService teamService;
+    private final PlayerService playerService;
 
-    public ManagerController(TeamService teamService) {
+    public ManagerController(TeamService teamService, PlayerService playerService) {
         this.teamService = teamService;
+        this.playerService = playerService;
     }
 
     @GetMapping("")
     public String managerHome(@AuthenticationPrincipal User user, Model model){
         model.addAttribute("managerHasTeam",teamService.hasManagerTeam(user));
-        return "manager";
+        return "manager/manager";
     }
 
     @GetMapping("/createTeam")
     public String createTeam(Team team,Model model){
         model.addAttribute("team",team);
-        return "createTeam";
+        return "manager/createTeam";
     }
 
     @PostMapping("/createTeam")
     public String createTeam(@Valid Team team, BindingResult errors, @AuthenticationPrincipal User user){
         if(errors.hasErrors()){
-            return "createTeam";
+            return "manager/createTeam";
         }
         for(Player player : team.getPlayers()){
             player.setTeam(team);
         }
         teamService.saveTeam(team,user);
-        return "redirect:/";
+        return "redirect:/manager/team";
+    }
+
+    @GetMapping("/team")
+    public String showTeam(@AuthenticationPrincipal User user,Model model){
+        model.addAttribute("team",teamService.findTeamByManager(user));
+        return "manager/team";
+    }
+
+    @GetMapping("/team/editPlayer/{id}")
+    public String updatePlayer(@PathVariable String id,Model model){
+        model.addAttribute("player",playerService.findPlayerById(Long.valueOf(id)));
+        return "manager/editPlayer";
+    }
+
+    @PostMapping("/team/editPlayer/{id}")
+    public String saveUpdatedPlayer(@PathVariable String id,@Valid Player player,BindingResult result){
+        if(result.hasErrors()){
+            return "manager/editPlayer";
+        }
+        playerService.updatePlayer(player,Long.valueOf(id));
+        return "redirect:/manager/team";
+    }
+
+    @GetMapping("/team/deletePlayer/{id}")
+    public String deletePlayer(@PathVariable String id){
+        playerService.deletePlayerById(Long.valueOf(id));
+        return "redirect:/manager/team";
+    }
+
+    @GetMapping("/team/addPlayer")
+    public String addPlayer(Model model,Player player){
+        model.addAttribute("player",player);
+        return "manager/addPlayer";
+    }
+
+    @PostMapping("/team/addPlayer")
+    public String saveAddedPlayer(@Valid Player player,BindingResult result,@AuthenticationPrincipal User user){
+        if(result.hasErrors()){
+            return "manager/addPlayer";
+        }
+        player.setTeam(teamService.findTeamByManager(user));
+        playerService.savePlayer(player);
+        return "redirect:/manager/team";
     }
 }
